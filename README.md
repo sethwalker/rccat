e12a3d51f3a21f75d6b48bc44930e0f0f7939a30
Seth Walker <seth.h.walker@gmail.com>
2024-06-24 09:23:35 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-an implementation of [irccat](https://github.com/RJ/irccat) but for Zulip
+An [irccat](https://github.com/RJ/irccat)-like for Zulip.
+
+`@rccat` listens for `?`-prefixed keywords in direct messages streams it is subscribed to, such as:
+
+```
+?weather 11201
+```
+
+It is a design goal to make adding new keyword match patterns _very_ easy - currently they can be added to the `match` statement in the `dispatch` function in `rccat.py`. Plugin system TBD, but should at a minimum support Zulip bots that could be run via [`zulip-run-bot`](https://zulip.com/api/running-bots).
+
+irccat is so named because in addition to reponding _within_ the chat network, one can also "cat" (usually `netcat` / `nc`) messages _into_ the chat network, for example
+
+```sh
+echo "#checkins>Seth+Walker Finished impossible stuff day with a [silly little tcp listener](https://github.com/sethwalker/rccat) that sends messages to zulip." | nc cat.recurse.com 12345
+```
+
+Newer implementations also support HTTP.
--- rccat.py
+++ rccat.py
@@ -2,16 +2,6 @@ import zulip
 from zulip_bots import lib
 
 
-def handle_message(message, bot_handler):
-    if message["content"].startswith("?"):
-        words = message["content"].split(" ")
-        command: str = message["content"].split(" ")[0]
-        dispatch(command.lstrip("?"), message, bot_handler)
-    else:
-        print("not a command")
-        print(message)
-
-
 def dispatch(command, message, bot_handler):
     # strip the command from the message
     message["content"] = message["content"][len(command) + 1 :]
@@ -20,14 +10,34 @@ def dispatch(command, message, bot_handler):
         case "help":
             bot_handler.send_reply(message, "you have been helped")
             return
+
+        case "list":
+            bot_handler.send_reply(message, "help, list, convert")
+
+        # proof of concept forwarding to other (local) zulip bots
+        # TBD?: forward via chat to other external bots
         case "convert":
             from handlers.bots.converter import converter
 
             handler = converter.handler_class()
             handler.handle_message(message, bot_handler)
 
+        case _:
+            # TODO: if direct message or mention, otherwise ignore
+            bot_handler.send_reply(message, "unknown command, try ?list")
+
 
 if __name__ == "__main__":
     client = zulip.Client(config_file="zuliprc")
     bot_handler = lib.ExternalBotHandler(client, None, None)
-    client.call_on_each_message(lambda msg: handle_message(msg, bot_handler))
+
+    def handle_message(message):
+        if message["content"].startswith("?"):
+            words = message["content"].split(" ")
+            command: str = message["content"].split(" ")[0]
+            dispatch(command.lstrip("?"), message, bot_handler)
+        else:
+            print("not a command")
+            print(message)
+
+    client.call_on_each_message(handle_message)
